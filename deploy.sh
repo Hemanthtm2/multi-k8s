5357dbb2b2395178f056548859cd3fdd81417548
hemanthtm2 <hemanthtm2@gmail.com>
2019-05-08 00:18:06 +0530

    tets

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
-docker build -t hemanthtm2/multi-client:latest -t hemanthtm2/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t hemanth/multi-client:latest -t hemanth/multi-client:$SHA -f ./client/Dockerfile ./client 
 
-docker build -t hemanthtm2/multi-server:latest -t hemanthtm2/multi-server:$SHA -f ./server/Dockerfile ./server 
+docker build -t hemanth/multi-server:latest -t hemanth/multi-server:$SHA -f ./server/Dockerfile ./server 
 
-docker build -t hemanthtm2/multi-worker:latest -t hemanthtm2/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t hemanth/multi-worker:latest -t hemanth/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push hemanthtm2/multi-client:latest
+docker push hemanth/multi-client:latest
 
-docker push hemanthtm2/multi-server:latest
+docker push hemanth/multi-server:latest
 
-docker push  hemanthtm2/multi-worker:latest
+docker push  hemanth/multi-worker:latest
 
-docker push hemanthtm2/multi-client:$SHA
+docker push hemanth/multi-client:$SHA
 
-docker push hemanthtm2/multi-server:$SHA
+docker push hemanth/multi-server:$SHA
 
-docker push  hemanthtm2/multi-worker:$SHA
+docker push  hemanth/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=hemanthtm2/multi-server:$SHA
+kubectl set image deployments/server-deployment server=hemanth/multi-server:$SHA
 
-kubectl set image deployments/client-deployment client=hemanthtm2/multi-client:$SHA
+kubectl set image deployments/client-deployment client=hemanth/multi-client:$SHA
 
-kubectl set image deployments/worker-deployment worker=hemanthtm2/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=hemanth/multi-worker:$SHA
